67c81b6e9646408bae60f3d922f171de71422796
Jake Torrance <jaket1234@hotmail.com>
2024-10-24 18:41:05 +0100

    Fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const base = env.VITE_GITHUB_PAGES !== undefined ? env.VITE_GITHUB_PAGES : "/";
+  return {
+    base: base,
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
